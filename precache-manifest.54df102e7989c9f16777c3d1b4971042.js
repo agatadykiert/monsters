008254f6d359629fec58ca7d1e133bffdfe16fac
adykiert <agata.dykiert@gmail.com>
2020-05-13 13:13:18 +0200

    Updates

---
+++ precache-manifest.54df102e7989c9f16777c3d1b4971042.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f8c985c0dde7e3a71925248d2d9b63f",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "ecb6e005d29bb84e95a4",
+    "url": "/monsters/static/css/main.a56fd416.chunk.css"
+  },
+  {
+    "revision": "f7b84cd810271619eca9",
+    "url": "/monsters/static/js/2.7d94dc6b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters/static/js/2.7d94dc6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ecb6e005d29bb84e95a4",
+    "url": "/monsters/static/js/main.300e9fee.chunk.js"
+  },
+  {
+    "revision": "77f8f01ea360998e236f",
+    "url": "/monsters/static/js/runtime-main.88ae1bce.js"
+  }
+]);
